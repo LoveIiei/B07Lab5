5a14b20c9c11ed46dac3ae53b859c57b0c512303
LoveIiei <huaweiyuan@hotmail.com>
2025-10-24 09:48:28 -0400

    feat: add rectangle class

---
+++ Rectangle.java
@@ -0,0 +1,34 @@
+package lab5;
+
+public class Rectangle {
+	double x;
+	double y;
+	
+	public Rectangle(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double area() {
+		return x * y;
+	}
+
+	@Override
+	public int hashCode() {
+		int hx = Double.hashCode(x);
+    	int hy = Double.hashCode(y);
+    	return 31 * hx + hy;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rectangle other = (Rectangle) obj;
+		return Double.Compare(this.x,  other.x) == 0.0 && Double.Compare(this.y,  other.y) == 0.0;
+	}
+}
