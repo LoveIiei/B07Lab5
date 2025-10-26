ca5d71c3f3df3e915b8a9bce79100e0fbbf27b67
BrysonC03 <brysonchiou03@gmail.com>
2025-10-26 11:42:16 -0400

    fix: update Circle.java with the full implementation

--- Circle.java
+++ Circle.java
@@ -1,4 +1,37 @@
+package lab5;
 
 public class Circle {
-
+	double radius;
+	
+	public Circle(double radius)
+	{
+		this.radius = radius;
+	}
+	
+	public double area()
+	{
+		return Math.PI*radius*radius;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		int inHashCode = (int) (radius * 1000);
+		int x = 3 + (5*inHashCode + 7);
+		int y = x * inHashCode + 13;
+		return y*inHashCode;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+			return true;
+		if(obj == null)
+			return false;
+		if(this.getClass() != obj.getClass())
+			return false;
+		Circle otherObj = (Circle) obj;
+		return Double.compare(this.radius, otherObj.radius) == 0;
+	}
 }
